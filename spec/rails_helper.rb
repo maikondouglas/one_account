38e668dea31f889a9f7c762b908c35b4c23b8afb
Maikon Douglas <maikon.douglas@vindi.com.br>
2023-07-12 19:03:28 -0300

    feat: configuration RSpec

---
+++ spec/rails_helper.rb
@@ -0,0 +1,26 @@
+require 'spec_helper'
+require_relative '../config/environment'
+require 'rspec/rails'
+
+ENV['RAILS_ENV'] ||= 'test'
+abort('The Rails environment is running in production mode!') if Rails.env.production?
+
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].sort.each { |f| require f }
+
+begin
+  ActiveRecord::Migration.maintain_test_schema!
+rescue ActiveRecord::PendingMigrationError => e
+  abort e.to_s.strip
+end
+RSpec.configure do |config|
+  config.around(:each) do |example|
+    Timeout.timeout(10) do
+      example.run
+    end
+  end
+
+  config.use_transactional_fixtures = true
+  config.infer_spec_type_from_file_location!
+  config.filter_rails_from_backtrace!
+  config.formatter = :progress
+end
