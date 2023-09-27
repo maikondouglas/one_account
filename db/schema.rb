b7bfdcf85e48b94f13ed27a9c3abbc74c201d765
Maikon Douglas <maikon.douglas@vindi.com.br>
2023-09-27 12:37:01 -0300

    test sqlite

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,6 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.0].define(version: 2023_09_25_032511) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "accounts", force: :cascade do |t|
     t.string "first_name", null: false
     t.string "last_name", null: false
